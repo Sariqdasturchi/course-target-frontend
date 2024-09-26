dbc265c80406f2da3f2c138acc55885b9b2e262d
Suhrob <suhrobgithub22@gmail.com>
2024-09-26 13:03:53 +0500

    mask

--- js/main.js
+++ js/main.js
@@ -1,16 +1,27 @@
 const form = document.getElementById('sheetdb-form')
-const cont = document.querySelector('section')
 const success = document.querySelector('.success')
 const danger = document.querySelector('.danger')
+const fullName = document.getElementById('phone')
+const phone = document.getElementById('lastName')
+console.log(fullName);
 
 
 form.addEventListener('submit', (e) => {
     e.preventDefault(); // Formani avtomatik jo'natilishini oldini olish
 
-    const fullName = form['lastName'].value.trim();
-    const phone = form['phone'].value.trim();
-
-    if (!fullName || !phone) {
+    if (fullName.value === '' || phone.value === '') {
+        danger.style.display = 'block'
+        success.style.display = 'none'
+        setTimeout(() => {
+            danger.style.display = 'none'
+        }, 4000);
+    } else if (fullName || phone.value === '') {
+        danger.style.display = 'block'
+        success.style.display = 'none'
+        setTimeout(() => {
+            danger.style.display = 'none'
+        }, 4000);
+    } else if (fullName.value === '' || phone) {
         danger.style.display = 'block'
         success.style.display = 'none'
         setTimeout(() => {
@@ -30,7 +41,7 @@ form.addEventListener('submit', (e) => {
                 }, 6000);
                 form.reset(); // Formani tozalash
             })
-
+      
     }
     console.log();
 });
